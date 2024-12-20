0a3758e36e12270323a25aebe9ffd4650aacd8de
Christian Brabandt <cb@256bit.org>
2024-12-20 09:01:52 +0100

    Add apache-fop 2.10

--- fop.sh
+++ fop.sh
@@ -4,6 +4,10 @@
 # also needed: MSYS2 and ghostscript:
 # Install using: pacman -S mingw-w64-x86_64-ghostscript
 # export PATH, add fop and java to it
-PATH=$PATH:/C/ProgramData/Oracle/Java/javapath:/c/users/cb14508/Downloads/fop-2.4-bin/fop-2.4/fop
+#
+# for Windows
+# PATH=$PATH:/C/ProgramData/Oracle/Java/javapath:/c/users/cb14508/Downloads/fop-2.4-bin/fop-2.4/fop
 
-fop.cmd $@
+# fop.cmd $@
+
+./fop-2.10/fop/fop $@
